f5a22d1f1a2adb0ceadcc9b6864dd646c38d7d27
Gourangi Taware <gourangi0309@gmail.com>
2025-01-04 03:33:38 -0600

    Login added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express"
 import cors from "cors"
+import connectDB from './config/db.js';
+import foodRouter from "./routes/foodRoutes.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
 
 const app =  express()
 const port = 4000;
-import connectDB from './config/db.js';
-import foodRouter from "./routes/foodRoutes.js";
+
 
 app.use(express.json())
 app.use(cors())
@@ -17,6 +20,7 @@ app.get("/", (req, res)=>{
 
 app.use('/api/food', foodRouter);
 app.use('/images', express.static('uploads'))
+app.use('/api/user', userRouter);
 
 app.listen(port, ()=>{
     console.log(`Server running on http://localhost:${port}`);
--- frontend/src/components/LoginPopup/Loginpopup.jsx
+++ frontend/src/components/LoginPopup/Loginpopup.jsx
@@ -1,22 +1,58 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Loginpopup.css'
 import { assets } from '../../assets/assets'
+import axios from "axios"
+import {toast} from "react-toastify"
+import { StoreContext } from '../../context/StoreContext'
 
 const loginpopup = ({setShowLogin}) => {
-    const [currState, setCurrentState] = useState("Login")
+    const {url, setToken} = useContext(StoreContext)
+    const [currState, setCurrentState] = useState("Login");
+    const [data, setData] = useState({
+        name:"",
+        email:"",
+        password:"",
+    });
+
+    const onChangeHandler = (event) =>{
+        const name = event.target.name;
+        const value = event.target.value;
+        setData((data)=>({ ...data, [name]:value}));
+    }
+
+    const onLogin = async(event) =>{
+        event.preventDefault();
+        let newUrl = url;
+        if(currState === "Login"){
+            newUrl += "/api/user/login"
+        }else{
+            newUrl += "/api/user/register"
+        }
+
+        const response = await axios.post(newUrl,data);
+        if(response.data.success){
+            setToken(response.data.token);
+            localStorage.setItem("token", response.data.token);
+            toast.success("Login Successfully")
+            setShowLogin(false);
+        }else{
+            toast.error(response.data.message);
+        }
+    }
+
   return (
     <div className='login-popup'>
-        <form className="login-popup-container">
+        <form onSubmit={onLogin} className="login-popup-container">
             <div className="login-popup-title">
                 <h2>{currState}</h2>
                 <img onClick={()=>setShowLogin(false)} src={assets.cross_icon}/>
             </div>
             <div className="login-popup-inputs">
-                {currState==="Login"?<></>:<input type='text' placeholder='Your Name' required/>}
-                <input type='text' placeholder='Your Email' required></input>
-                <input type='text' placeholder='Password' required></input>
+                {currState==="Login"?<></>:<input name="name" value={data.name} onChange={onChangeHandler} type='text' placeholder='Your Name' required/>}
+                <input type='text' name="email" value={data.email} onChange={onChangeHandler} placeholder='Your Email' required></input>
+                <input type='text' name="password" value={data.password} onChange={onChangeHandler} placeholder='Password' required></input>
             </div>
-            <button>{currState==="Sign Up"? "Create Account":"Login" }</button>
+            <button type='submit'>{currState==="Sign Up"? "Create Account":"Login" }</button>
             <div className="login-popup-condition">
                 <input type='checkbox' required/>
                 <p>By continuing, i agree to the terms of use & privacy policy.</p>
